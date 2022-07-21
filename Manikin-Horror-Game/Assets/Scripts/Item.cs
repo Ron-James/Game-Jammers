ed12185cb99f7fb3d599a5ff8ed284074f66f4fd
RonJames36 <jajotheron@gmail.com>
2022-07-21 14:08:08 +0200

    Room Added

---
+++ Manikin-Horror-Game/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item", order = 1)]
+public class Item : ScriptableObject
+{
+
+    public string itemName;
+    public string description;
+    public GameObject prefab;
+
+
+
+    
+
+}
